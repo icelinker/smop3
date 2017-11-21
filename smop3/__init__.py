81cc6904eb8ac0564a57edd74854efabbf229b09
Linghui Yang <icelinker@gmail.com>
2017-11-21 00:04:43 +0000

    Initial commit

---
+++ smop3/__init__.py
@@ -0,0 +1,7 @@
+# SMOP compiler -- Simple Matlab/Octave to Python compiler
+# Copyright 2011-2014 Victor Leikehman
+from . import * 
+from . import version
+from . import yacc
+from . import parse,resolve,backend,main
+from .version import __version__
